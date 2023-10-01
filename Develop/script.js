466d20abaa7fb53990183f0bff09227c8885c0cd
Devin Shade <devinshade@Devins-MacBook-Pro.local>
2023-10-01 15:34:16 -0600

    added code for pw character length, uppercase, and lowercase confirm

--- Develop/script.js
+++ Develop/script.js
@@ -11,9 +11,41 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("Confirm button click works")
+// Input variables
+var input = [""]
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var randomPassword = ""
 
+function generatePassword() {
+  console.log("Confirm button click works");
+// 1. Password length = 8-128 characters
+  var passwordLength = prompt("Choose password length from 8-128 characters:");
+// Validate password length
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    window.alert("Password must be between 8-128 characters.");
+    return
+  }
+// 2. Lowercase, uppercase, numbers, special characters
+    var includeLowercase = confirm("Include lowercase characters?");
+    // confirm lowercase
+    if (includeLowercase === true) {
+      for (var i = 0; i < lowercase.length; i++) {
+        input.push(lowercase[i]);
+      }
+      console.log(input);
+    }
+
+    var includeUppercase = confirm("Include uppercase characters?")
+    // confirm uppercase
+    if (includeUppercase === true) {
+      for (var i = 0; i < uppercase.length; i++) {
+        input.push(uppercase[i]);
+      }
+      console.log(input)
+    }
 
 // ! TODO: Display password in box
   return "Generated password will go here"
